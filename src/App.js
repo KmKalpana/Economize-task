8a988af5e3acf8f2d9f6b4b0f910b9fa08c5c594
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2023-02-15 20:48:54 +0530

    installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div className='text-3xl font-bold underline'>
+       Hello World
     </div>
   );
 }
