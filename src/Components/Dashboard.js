d0db8c89afb607405a86864636a92981801108db
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2023-02-16 10:05:37 +0530

    Boxes Completed

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,8 +4,8 @@ import { GrFormSchedule } from 'react-icons/gr'
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 const Dashboard = () => {
   return (
-    <div className="p-8">
-      <div className="grid grid-cols-3 gap-8">
+    <div className="pt-1">
+      <div className="p-8 grid grid-cols-3 gap-8">
         {/** Column-1 */}
         <div className="flex">
           <div className="m-1 mr-2 w-12 h-12 relative flex justify-center items-center rounded-full bg-gray-500 text-xl text-white">
@@ -46,9 +46,46 @@ const Dashboard = () => {
             </div>
           </div>
         </div>  {/** Column-2 Completed*/}
-        {/** Boxes */}
-        
       </div>
+       {/** Boxes */}
+        <div className="pl-9 grid grid-cols-4 divide-x gap-6">
+             <div>   {/**SubBox-1 */}
+             <div className='flex py-1'>
+             <span className='text-gray-500 pr-4'>Total Revenue</span>
+             <span className='rounded-full border pl-2 pr-3 bg-green-200 text-green-700 font-semibold'>+53.2%</span>
+             </div>
+             <span className='font-bold text-3xl flex text-center  py-1'>$32,621.72</span>
+             <span className='text-gray-500  py-1'>433 Orders</span>
+             </div> {/**SubBox-1 Completed*/}
+              <div className='pl-6'>   {/**SubBox-2 */}
+             <div className='flex py-1'>
+             <span className='text-gray-500 pr-4'>Subscribers</span>
+             <span className='rounded-full border pl-2 pr-3 bg-green-200 text-green-700 font-semibold'>+32.5%</span>
+             </div>
+             <span className='font-bold text-3xl flex text-center  py-1'>$484,205</span>
+             <span className='text-gray-500  py-1'>$56 Average Order</span>
+             </div> {/**SubBox-2 Completed*/}
+              <div className='pl-6'>   {/**SubBox-3 */}
+             <div className='flex py-1'>
+             <span className='text-gray-500 pr-4'>Conversations</span>
+             <span className='rounded-full border pl-2 pr-3 bg-red-200 text-red-700 font-semibold'>-3.4%</span>
+             </div>
+             <span className='font-bold text-3xl flex text-center  py-1'>$552,33</span>
+             <span className='text-gray-500  py-1'>5 min Average Response time</span>
+             </div> {/**SubBox-3 Completed*/}
+              <div className='pl-6'>   {/**SubBox-4 */}
+             <div className='flex py-1'>
+             <span className='text-gray-500 pr-2'>Popup Conversation Rate</span>
+             <span className='rounded-full border pl-2 pr-3 bg-green-200 text-green-700 font-semibold'>+32.5%</span>
+             </div>
+             <span className='font-bold text-3xl flex text-center  py-1'>25%</span>
+             <span className='text-gray-500  py-1'>5% Sale Conversation Rate</span>
+             </div> {/**SubBox-4 Completed*/}
+          </div>
+        {/** Boxes End */}
+        <div>  {/** Chart.js Started*/}
+        
+        </div> {/**Chart Js Ended */}
     </div>
   )
 }
